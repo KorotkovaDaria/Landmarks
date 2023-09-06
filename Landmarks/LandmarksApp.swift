89e5ca73eb5846b0d01c1b60f0885743606b835d
Dqueenprinsees <130159500+Dqueenprinsees@users.noreply.github.com>
2023-09-06 12:51:20 +0300

    First commit
    First commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Daria on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
